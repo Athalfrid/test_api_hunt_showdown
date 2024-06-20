7ee7ce76f00b52365b28eb79c18cdc649c917583
Aurélien Robin <111597427+Athalfrid@users.noreply.github.com>
2024-06-20 16:00:41 +0200

    First commit

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const mongoose = require("mongoose");
+const userRoutes = require('../src/user/routes/userRoutes');
+
+const {password} = require('../config.json')
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+mongoose.connect(`mongodb+srv://athalfrid:${password}@cluster0.rfcopg5.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
+.then( ()=> {
+  console.log('MongoDB connection established');
+})
+.catch(err => console.error('Could not connect to MongoDB ...', err));
+
+app.use(bodyParser.json());
+app.use(cors());
+
+app.use('/api/v1/users',userRoutes)
+
+app.listen(port, () => {
+  console.log(`Server listening on ${port}`);
+});
---
+++ src/user/models/userModels.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        minLength: 3,
+        maxLength: 255
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 3,
+        maxLength: 1024
+    },
+},
+{
+    timestamps: true
+});
+
+const User = mongoose.model('User',userSchema);
+
+module.exports = User;
